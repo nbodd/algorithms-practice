4f3618f52fff53dfd7bdaa36dcdf95517fe83eaf
nickbodd <nick.bodd90@gmail.com>
2016-02-05 14:10:04 -0500

    Greedy algorithms : priyanka toys & two arrays sum less than k

--- pyalgorithms/practice.py
+++ pyalgorithms/practice.py
@@ -30,16 +30,93 @@ def sherlockArray() :
         else:
             print 'NO'
 
-def main():
-    V = int(raw_input())
-    n = int(raw_input())
+def sherlock_and_minimax(A, P, Q):
+    A.sort()
+
+    pValues = [abs(val - P) for val in A]
+    print pValues
+    pMin = min(pValues)
+
+    qValues = [abs(val - Q) for val in A]
+    print qValues
+    qMin = min(qValues)
+
+    firstP = P
+    firstQ = P
+    firstPIndex = 0
+    for index in range(len(A)):
+        if (P >= A[index]):
+            firstP = A[index]
+            firstPIndex = index
+
+        if (Q >= A[index]):
+            firstQ = A[index]
+
+    if (firstP == firstQ):
+        firstQ = A[firstPIndex + 1]
+
+    median = (firstP + firstQ) / 2
+    print firstP
+    print firstQ
+    print median
+    medianValues = [abs(median - val) for val in A]
+    print medianValues
+
+    medianMin = min(medianValues)
+
+
+    if pMin >= qMin and pMin >= medianMin:
+        return P
+    elif medianMin >= qMin and medianMin > pMin:
+        return median
+    else :
+        return Q
+
+def sherlock_minimax_main():
+    N = int(raw_input())
 
     A = map(int, (raw_input().split(' ')))
-    print A
-    for i in range(n):
-        if A[i] == V:
-            print i
-            break
+    P, Q = map(int, raw_input().split())
+    B = sherlock_and_minimax(A, P, Q)
+    print B
+
+def two_arrays_main() :
+    testCases = int(raw_input())
+    for i in range(testCases):
+        [N, K] = map(int, raw_input().split(' '))
+
+        A = map(int, raw_input().split(' '))
+        B = map(int, raw_input().split(' '))
+
+        A.sort()
+        B.sort(reverse=True)
+
+        found = True
+        for i in range(N):
+            if (A[i] + B[i] < K):
+                found = False
+                break
+
+        if found:
+            print 'YES'
+        else:
+            print 'NO'
+
+def priyanka_main():
+    N = int(raw_input())
+    A = map(int, raw_input().split(' '))
+    A.sort()
+    count = 1
+    current = A[0]
+    for index in range(1, len(A)):
+        if A[index] <= (current + 5):
+            continue
+        else:
+            count += 1
+            current = A[index]
+
+    print count
+
 
 if __name__ == '__main__' :
     main()
