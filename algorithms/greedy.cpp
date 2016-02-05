71c0e08737d338c1bc65a3ac84419d7b2673577f
nickbodd <nick.bodd90@gmail.com>
2016-02-05 02:04:48 -0500

    cycle detection in linked list

---
+++ algorithms/greedy.cpp
@@ -0,0 +1,9 @@
+//
+//  greedy.cpp
+//  algorithms
+//
+//  Created by Nick on 2/5/16.
+//  Copyright © 2016 Nick. All rights reserved.
+//
+
+#include <stdio.h>
--- algorithms/linked_list.cpp
+++ algorithms/linked_list.cpp
@@ -21,6 +21,15 @@ struct ListNode {
     }
 };
 
+void print_list(shared_ptr<ListNode> list) {
+    cout << "LIST : ";
+    while (list != nullptr) {
+        cout << list->data << " -> ";
+        list = list->next;
+    }
+    cout << endl;
+}
+
 //A & B are already sorted
 shared_ptr<ListNode> merge_lists(shared_ptr<ListNode> A, shared_ptr<ListNode> B) {
     if (A == nullptr) {
@@ -70,15 +79,6 @@ shared_ptr<ListNode> merge_lists(shared_ptr<ListNode> A, shared_ptr<ListNode> B)
     }
 }
 
-void print_list(shared_ptr<ListNode> list) {
-    cout << "LIST : ";
-    while (list != nullptr) {
-        cout << list->data << " -> ";
-        list = list->next;
-    }
-    cout << endl;
-}
-
 void merge_lists_test() {
     shared_ptr<ListNode> A = nullptr;
     shared_ptr<ListNode> B = nullptr;
@@ -102,9 +102,94 @@ void merge_lists_test() {
     print_list(merge_lists(E0, D0));
 }
 
-int main() {
+shared_ptr<ListNode> detect_loop(shared_ptr<ListNode> list) {
+    shared_ptr<ListNode> slow = list;
+    shared_ptr<ListNode> fast = list;
+    shared_ptr<ListNode> meetNode = nullptr;
     
-    merge_lists_test();
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+        
+        if (slow == fast) {
+            meetNode = slow;
+            break;
+        }
+    }
+    
+    if (fast == nullptr || fast->next == nullptr) {
+        return nullptr;
+    }
+    
+    int radius = 0;
+    do {
+        slow = slow->next;
+        radius++;
+    } while (slow != meetNode);
+    
+    slow = list;
+    fast = list;
+    
+    int count = 0;
+    while (count < radius) {
+        fast = fast->next;
+        count++;
+    }
+    
+    while (slow != fast) {
+        slow = slow->next;
+        fast = fast->next;
+    }
+    
+    return slow;
+    
+}
+
+void loop_tests() {
+    
+    cout << (detect_loop(nullptr) == nullptr ? "NULL PTR" : "WRONG ANSWER") << endl;
+    
+    shared_ptr<ListNode> C = nullptr;
+    shared_ptr<ListNode> D3 = make_shared<ListNode>(9, nullptr);
+    shared_ptr<ListNode> D2 = make_shared<ListNode>(6, D3);
+    shared_ptr<ListNode> D1 = make_shared<ListNode>(5, D2);
+    shared_ptr<ListNode> D0 = make_shared<ListNode>(3, D1);
+    
+    cout << (detect_loop(D0) == nullptr ? "NO LOOP" : "WRONG ANSWER") << endl;
+    
+    shared_ptr<ListNode> list[100];
+    for (int i=0; i<100; ++i) {
+        list[i] = make_shared<ListNode>(i, nullptr);
+    }
+    
+    for (int i=0; i<99; ++i) {
+        list[i]->next = list[i+1];
+    }
+    
+    list[99]->next = list[10];
+    cout << (detect_loop(list[0])->data == list[10]->data ? "CORRECT" : "WRONG ANSWER") << endl;
+    
+    list[99]->next = list[0];
+    cout << (detect_loop(list[0])->data == list[0]->data ? "CORRECT" : "WRONG ANSWER") << endl;
+    
+    list[99]->next = list[95];
+    cout << (detect_loop(list[0])->data == list[95]->data ? "CORRECT" : "WRONG ANSWER") << endl;
+    
+    list[99]->next = list[85];
+    cout << (detect_loop(list[0])->data == list[85]->data ? "CORRECT" : "WRONG ANSWER") << endl;
+    
+    list[99]->next = list[15];
+    cout << (detect_loop(list[0])->data == list[15]->data ? "CORRECT" : "WRONG ANSWER") << endl;
+    
+    list[99]->next = list[50];
+    cout << (detect_loop(list[0])->data == list[50]->data ? "CORRECT" : "WRONG ANSWER") << endl;
+    
+}
+
+int linked_list_main() {
+    
+    //merge_lists_test();
+    loop_tests();
     
     return 0;
 }
